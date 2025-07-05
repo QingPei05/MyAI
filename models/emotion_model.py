0f28d9ba5217834f0079ffb4f8fa9a2993a2e75a
Qing Pei <qingpei050810@gmail.com>
2025-07-05 22:29:58 +0800

    Update emotion_model.py

--- models/emotion_model.py
+++ models/emotion_model.py
@@ -6,9 +6,6 @@ class EmotionModel:
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
     def detect_emotion(self, image):
-        """
-        检测图片中的情感。
-        """
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
