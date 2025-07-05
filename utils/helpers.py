ccea5db4dd4eec1d86abf1b2fdcd4ac5d4c31bb2
Qing Pei <qingpei050810@gmail.com>
2025-07-06 00:27:33 +0800

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,8 @@
+from functools import lru_cache
+import hashlib
 
+def cached_analysis(func):
+    @lru_cache(maxsize=100)
+    def wrapper(image_data: bytes, *args, **kwargs):
+        return func(image_data, *args, **kwargs)
+    return wrapper
